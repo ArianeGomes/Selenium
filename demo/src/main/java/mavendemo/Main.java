1fcbc4bf3a5e5b65d764aeaaf52c6abfa4b30a2a
Ariane Gomes <ariane@estudante.ufscar.br>
2024-08-05 12:32:41 -0300

    Adding more browsers drivers

--- demo/src/main/java/mavendemo/Main.java
+++ demo/src/main/java/mavendemo/Main.java
@@ -2,13 +2,17 @@ package mavendemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
+        // To run on Chrome Browser
         WebDriver driver = new ChromeDriver();
+        // To run on Firefox Browser
+        //WebDriver driver = new FirefoxDriver();
+        // To run on Edge Browser
+        //WebDriver driver = new EdgeDriver();
 
         driver.get("https://rahulshettyacademy.com/");
 
